595de9158ffbf45b1614cfcbf461b30f7f3e7d4e
EsleySaab <esleysantana.sa@gmail.com>
2024-10-30 22:12:22 -0300

    adding validation

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ const rangeEnd = document.getElementById("number-input-3").value || 0
 document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault()
 })
+
+if (numberCount <= 0 || rangeStart >= rangeEnd) {
+  document.getElementById("result").innerHTML =
+    "<p class='text-danger'>Por favor, insira valores válidos.</p>"
+  return
+}
