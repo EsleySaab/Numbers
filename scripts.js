c7d36f2d70e5ea4686f137e8710415088149811f
EsleySaab <esleysantana.sa@gmail.com>
2024-10-30 22:05:02 -0300

    obtaining input values

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const numberCount = document.getElementById("number-input-1").value || 0
+const rangeStart = document.getElementById("number-input-2").value || 0
+const rangeEnd = document.getElementById("number-input-3").value || 0
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault()
+})
